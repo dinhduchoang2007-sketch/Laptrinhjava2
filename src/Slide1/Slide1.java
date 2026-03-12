fb9ca0940d205b4384f2206e0ad5149984ff3b7e
dinhhoang27 <dinhduchoang2007@gmail.com>
2026-03-12 19:16:14 +0700

    Update Slide1.java

--- src/Slide1/Slide1.java
+++ src/Slide1/Slide1.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 public class Slide1 {
 	public static void main(String[] args) {
         System.out.println("17 PS48474 Đinh Đức Hoàng");
-//       Xe xe = new Xe();
-//        xe.setHangXe("Toyota");
-//        xe.setNamSanXuat(2026);
-//        System.out.println(xe.toString());
+      Xe xe = new Xe();
+       xe.setHangXe("Toyota");
+       xe.setNamSanXuat(2026);
+       System.out.println(xe.toString());
         
-//        Xe xe2 = new Xe("Honda",2000);
-//        System.out.println(xe2.toString());
-//        MayTinh mayTinh = new MayTinh();
-//        System.out.println(mayTinh.tinhTong(3,5));
-//        System.out.println(mayTinh.tinhTong(3.5 , 5) );
-//        System.out.println(mayTinh.tinhTong(3, 5 ,6));
+       Xe xe2 = new Xe("Honda",2000);
+       System.out.println(xe2.toString());
+       MayTinh mayTinh = new MayTinh();
+       System.out.println(mayTinh.tinhTong(3,5));
+       System.out.println(mayTinh.tinhTong(3.5 , 5) );
+       System.out.println(mayTinh.tinhTong(3, 5 ,6));
         
         ArrayList<Animal> listAnimal = new ArrayList<Animal>();
         listAnimal.add(new Dog("Phúc",90));
